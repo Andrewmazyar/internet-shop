bd085718b66e8a4103bc8b345628957c7a9f30ad
Andrewmazyar <maziarandriy@gmail.com>
2020-05-04 10:32:20 +0300

    added losted function

--- src/main/java/mate/academy/internetshop/dao/UserDao.java
+++ src/main/java/mate/academy/internetshop/dao/UserDao.java
@@ -1,7 +1,9 @@
 package mate.academy.internetshop.dao;
 
+import java.util.Optional;
 import mate.academy.internetshop.model.User;
 
 public interface UserDao extends InterfaceDao<User, Long> {
 
+    Optional<User> findByLogin(String login);
 }
