94c99722cf4893e84a69036fb6d0fd25632f8071
Andrewmazyar <maziarandriy@gmail.com>
2020-06-01 14:04:49 +0300

    add travis and add method getByUserId for ShoppingCartJdbcImpl

--- src/main/java/web/internetshop/dao/ShoppingCartDao.java
+++ src/main/java/web/internetshop/dao/ShoppingCartDao.java
@@ -1,6 +1,8 @@
 package web.internetshop.dao;
 
+import java.util.Optional;
 import web.internetshop.model.ShoppingCart;
 
 public interface ShoppingCartDao extends InterfaceDao<ShoppingCart, Long> {
+    public Optional<ShoppingCart> getByUserId(Long id);
 }
